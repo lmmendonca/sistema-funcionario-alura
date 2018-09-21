850682d206ae4cb374a0fc94f08027cf9950c8c6
lmaximino <lmaximino.mendonca@gmail.com>
2018-09-21 17:27:20 -0300

    remove .idea

---
+++ src/main/java/Designer.java
@@ -0,0 +1,11 @@
+public class Designer extends Funcionario{
+
+    public Designer(String nome, String cpf, double salario) {
+        super(nome, cpf, salario);
+    }
+
+    public double getBonificacao() {
+        return super.getSalario() * 0.5;
+    }
+
+}
